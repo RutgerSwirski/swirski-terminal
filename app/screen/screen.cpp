d4d94187d00dc94ece09c587192cabc66829f1b5
rutgerswirski <RutgerSwirski@users.noreply.github.com>
2026-07-11 22:09:55 +0200

    playing around with LVGL rendering home screen

--- app/screen/screen.cpp
+++ app/screen/screen.cpp
@@ -71,6 +71,70 @@ namespace swirski::screen
             LV_ALIGN_TOP_MID,
             0,
             15);
+
+        // create a flex container
+
+        lv_obj_t *flexContainer = lv_obj_create(screenRoot);
+
+        lv_obj_set_layout(flexContainer, LV_LAYOUT_FLEX);
+
+        lv_obj_set_flex_flow(flexContainer, LV_FLEX_FLOW_COLUMN);
+
+        lv_obj_set_size(flexContainer, 200, 150);
+
+        lv_obj_set_style_bg_color(flexContainer, lv_color_hex(0x00283d), LV_PART_MAIN);
+
+        lv_obj_set_flex_align(flexContainer, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER);
+
+        lv_obj_align(
+            flexContainer,
+            LV_ALIGN_CENTER,
+            0,
+            0);
+
+        // create new label Notifications
+        lv_obj_t *notifications =
+            lv_label_create(flexContainer);
+
+        lv_label_set_text(notifications, "> Notifications");
+
+        lv_obj_set_style_text_color(
+            notifications,
+            lv_color_white(),
+            LV_PART_MAIN);
+
+        // create new label Music
+        lv_obj_t *music =
+            lv_label_create(flexContainer);
+
+        lv_label_set_text(music, "Music");
+
+        lv_obj_set_style_text_color(
+            music,
+            lv_color_white(),
+            LV_PART_MAIN);
+
+        // create new label Studio
+        lv_obj_t *studio =
+            lv_label_create(flexContainer);
+
+        lv_label_set_text(studio, "Studio");
+
+        lv_obj_set_style_text_color(
+            studio,
+            lv_color_white(),
+            LV_PART_MAIN);
+
+        // create new label Settings
+        lv_obj_t *settings =
+            lv_label_create(flexContainer);
+
+        lv_label_set_text(settings, "Settings");
+
+        lv_obj_set_style_text_color(
+            settings,
+            lv_color_white(),
+            LV_PART_MAIN);
     }
 
     void showScreen(Screen screen)
--- desktop/lv_conf.h
+++ desktop/lv_conf.h
@@ -6,6 +6,7 @@
 #define LV_USE_OS LV_OS_NONE
 
 #define LV_USE_SDL 1
+#define LV_USE_FLEX 1
 
 #define LV_USE_LOG 1
 #define LV_LOG_LEVEL LV_LOG_LEVEL_WARN
