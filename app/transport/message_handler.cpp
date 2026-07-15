fd6f1465d74e1ac667199e450f39844cfe4f63d5
rutgerswirski <RutgerSwirski@users.noreply.github.com>
2026-07-15 15:35:35 +0200

    Remove message handler and message definitions for transport layer

--- app/transport/message_handler.cpp
+++
@@ -1,4 +0,0 @@
-
-
-// this file handles the incoming messages, after they have been passe from the correct transport layer eg bluetooth or websocket
-
--- app/transport/message_handler.hpp
+++
@@ -1,9 +0,0 @@
-// this files defines the message handler for the transport layer - eg bluetooth or websocket
-#pragma once
-
-#include <string>
-
-namespace swirski::transport::message_handler
-{
-    void handleMessage(std::string message);
-}
